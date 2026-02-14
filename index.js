88b8b78f7f848678d6110ff562b218714d463833
bvvivek6 <vivekbv242813@gmail.com>
2026-02-14 07:37:06 +0530

    fix: update log message to indicate GitHub Actions integration

--- index.js
+++ index.js
@@ -10,5 +10,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port with github actions added ${PORT}`);
 });
